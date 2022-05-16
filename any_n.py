766bbf52f572cd6facd71d76a3ca67d8c2b6fd3b
Martin <32459243+Muartin@users.noreply.github.com>
2022-05-16 18:41:10 -0400

    Made changes so that plots are now titled, and generated more figures for the figures folder.

--- any_n.py
+++ any_n.py
@@ -1,23 +1,21 @@
-# imports and such
-
 from scipy import integrate
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# defining the system of differential equations from 4.1 for n=3
+# defining the system of differential equations from 4.1 for any n
 def system(t, w, k, kstar, lmbda, M):
         derivatives = []
         n = len(w)      
 
-        #calculate for number of monomers derivative
+        #calculate for change in number of monomer w.r.t. time
         dot_w = 0
         for j in range(n - 1):
                 dot_w -= k*w[0]*w[j]
         dot_w += -kstar*w[0]*w[-1] - M*w[0] + lmbda
         derivatives.append(dot_w)
 
-        #calculate for number of soluble oligomers derivative
+        #calculate for change in number of soluble oligomers w.r.t time
         for s in range(1, n - 1):
                 dot_w = 0
                 for i in range(n):
@@ -31,7 +29,7 @@ def system(t, w, k, kstar, lmbda, M):
                 dot_w += -kstar*w[s]*w[-1] - M*w[s]
                 derivatives.append(dot_w)
 
-        #calcualate for number of senile plaques derivative
+        #calcualate for change in number of senile plaques w.r.t time
         dot_w = 0
         for i in range(n - 1):
                 for j in range(n - 1):
@@ -49,7 +47,7 @@ n = int(input('input a value for n: '))
 tf = 500
 dt = 0.1
 
-# initial point to solve with
+# initial point to solve with, all initial values are set to 0
 initial = tuple([0]*n)
 
 # defining constants
@@ -64,7 +62,7 @@ sol = integrate.solve_ivp( system, (0, tf), initial, t_eval=np.arange(0, tf + dt
 
 
 # plotting as a function of t
-# plt.title(title)
+plt.title('Plot for n=' + str(n))
 for i in range(n):
         this_label = 'Y' + str(i + 1)
         plt.plot(sol.t, sol.y[i], label=this_label)
